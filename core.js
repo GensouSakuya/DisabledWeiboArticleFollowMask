1d531038b291d264c7e93019d8117bd42bdb3724
幻葬咲夜 <zyqwert2@gmail.com>
2019-03-16 22:11:36 +0800

    Update core.js

--- core.js
+++ core.js
@@ -10,6 +10,6 @@
 // ==/UserScript==
 $('.W_gotop.S_ficon_bg').before('<a href="javascript:" id="gogoda" data-tip="text=阅读全文&amp;showWithAni=fadeInRight&amp;hideWithAni=fadeOutLeft&amp;pos=left-middle" class="W_gotop S_ficon_bg" action-type="data-tip" style="margin-bottom: 167px;position: fixed;top: auto;"><em class="W_ficon ficon_backtop S_bg2_c">🐔</em></a>')
 $("#gogoda").click(function(){
-    $('.WB_editor_iframe')[0].style['height'] = 'auto';
+    $('.WB_editor_iframe_new')[0].style['height'] = 'auto';
     $('.btn_line.W_tc.W_f14').remove()
 })
