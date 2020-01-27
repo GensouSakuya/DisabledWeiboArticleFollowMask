6158dd383e0675618e401049a123b01300b35428
幻葬咲夜 <zyqwert2@gmail.com>
2020-01-27 15:38:22 +0800

    fix

--- core.js
+++ core.js
@@ -10,6 +10,15 @@
 // ==/UserScript==
 $('.W_gotop.S_ficon_bg').before('<a href="javascript:" id="gogoda" data-tip="text=阅读全文&amp;showWithAni=fadeInRight&amp;hideWithAni=fadeOutLeft&amp;pos=left-middle" class="W_gotop S_ficon_bg" action-type="data-tip" style="margin-bottom: 167px;position: fixed;top: auto;"><em class="W_ficon ficon_backtop S_bg2_c">🐔</em></a>')
 $("#gogoda").click(function(){
-    $('.WB_editor_iframe_new')[0].style['height'] = 'auto';
+    var iframe1 = $('.WB_editor_iframe_new')
+    if(iframe1 != null && iframe1[0]!= null)
+    {
+        iframe1[0].style['height'] = 'auto';
+    }
+    var iframe2 = $('.WB_editor_iframe_word')
+    if(iframe2 != null && iframe2[0]!= null)
+    {
+        iframe2[0].style['height'] = 'auto';
+    }
     $('.btn_line.W_tc.W_f14').remove()
 })
